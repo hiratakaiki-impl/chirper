0a9e8cce10e664603dcddb8135f7b050fdac500d
hiratakaiki-impl <hirata.kaiki@impl.co.jp>
2026-09-14 13:32:35 +0900

    7-4: Define Relationships

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -4,9 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 #[Fillable(['message'])]
 class Chirp extends Model
 {
-    //
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
